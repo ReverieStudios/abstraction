80603580cc8398c7d3b8b4e3e0f2f255c5b2b810
Max Metzger <metzger.max@gmail.com>
2024-03-10 21:59:34 -0400

    adding a bit more logic to the redirect result

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,17 +1,20 @@
-import { getRedirectResult, type UserCredential } from 'firebase/auth';
+import { browserPopupRedirectResolver, getRedirectResult, type UserCredential } from 'firebase/auth';
 import { auth, handleSignIn } from '$lib/firebase';
+import { browser } from '$app/environment';
 
 /** @type { PageLoad } from "./$types"; */
 export async function load({ parent }) {
-    console.log("+page.ts checking for a redirect result");
-    try {
-        const result: UserCredential | null = await getRedirectResult(auth);
-        if (result) {
-            console.log("+page found a redirect result, handling sign-in");
-            await handleSignIn(result);
+    if (browser) {
+        console.log("+page.ts checking for a redirect result, auth is ", auth);
+        try {
+            const result: UserCredential | null = await getRedirectResult(auth, browserPopupRedirectResolver);
+            if (result) {
+                console.log("+page found a redirect result, handling sign-in");
+                await handleSignIn(result);
+            }
         }
-    }
-    catch (e) {
-        console.log(e);
+        catch (e) {
+            console.log(e);
+        }
     }
 }

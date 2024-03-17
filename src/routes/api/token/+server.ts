d70c3db92d67342f4c87bd75d62d7337edabf427
Max Metzger <metzger.max@gmail.com>
2024-03-17 16:41:24 -0400

    Applying a token to unlock a game (or other role) now automatically updates the cookie, so users do not have to refresh for the new roles to take effect

--- src/routes/api/token/+server.ts
+++ src/routes/api/token/+server.ts
@@ -1,33 +1,5 @@
-import { database } from '$lib/database';
-import { auth } from '$lib/firebase';
 import { json, type RequestHandler } from '@sveltejs/kit';
-import cookie from 'cookie';
-import jwt from 'jsonwebtoken';
-
-const loadUserData = async (token: string): Promise<{ userToken: string; userData: Object }> => {
-	const decoded = await auth.decodeToken(token);
-	if (decoded && decoded.uid) {
-		const { uid, email, name = email } = decoded;
-		const user = await database.users.doc(decoded.uid)?.read();
-		let userData = null;
-		if (!user.exists) {
-			userData = { name, email, roles: { system: 0, games: {} }, flags: {} };
-			await user.save(userData);
-		} else {
-			userData = user.data;
-		}
-
-		userData.uid = uid;
-
-		const privateKey = (import.meta.env.VITE_JWT_KEY ??
-			'471207445716c8fbc32d3ad0c39aab60a762bdb7') as string; // ensure some basic key to avoid error
-		return {
-			userToken: jwt.sign(userData, privateKey),
-			userData
-		};
-	}
-	return { userToken: '', userData: {} };
-};
+import { loadUserData } from 'lib/token';
 
 export const POST: RequestHandler = async ({request, cookies}) => {
 	const payload = await request.json();

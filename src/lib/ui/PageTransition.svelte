1d10b4259f91ebd8a8b6a9058a1664ec6e7a887b
Max Metzger <metzger.max@gmail.com>
2026-04-13 23:26:32 -0400

    reverting UI fix as it broke the animations

--- src/lib/ui/PageTransition.svelte
+++ src/lib/ui/PageTransition.svelte
@@ -1,34 +1,13 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
-	import { page } from '$app/stores';
 
-	function outfly(node: Element, params: Parameters<typeof fly>[1]) {
-		const anim = fly(node, params);
-		const duration = params?.duration ?? 300;
-		// If the transition gets orphaned (never removed from DOM), hide it after it should have finished
-		const timer = setTimeout(() => {
-			(node as HTMLElement).style.display = 'none';
-		}, duration + 100);
-		return {
-			...anim,
-			destroy() {
-				clearTimeout(timer);
-			}
-		};
-	}
+	export let data;
 </script>
 
-{#key $page.route.id}
+{#key data.url}
 	<div 
 		in:fly|global={{ y: -50, duration: 250, delay: 300 }}
-		out:outfly|global={{ y: -50, duration: 250 }}
-		class="transition-page">
+		out:fly|global={{ y: -50, duration: 250 }} class="transition-page">
 		<slot />
 	</div>
 {/key}
-
-<style>
-	.transition-page {
-		display: block;
-	}
-</style>
